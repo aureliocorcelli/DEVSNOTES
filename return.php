2e4165f2a414a5f0081990ddea7fb2767cf1dcbf
Aurelio Corcelli <aureliocorcelli@gmail.com>
2023-05-22 19:35:59 -0300

    atualizando retorno.php

--- return.php
+++ return.php
@@ -1,6 +1,6 @@
 <?php
-    header("Acess-Control-Allow-Origin: *");
-    header("Acess-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Content-Type: application/json");
     echo json_encode($array);
-    exit;
+exit;
